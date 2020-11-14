e707e0ee9c424c14508edb492a5f346e63d92012
Igor Pidik <igorPidik@gmail.com>
2020-11-14 22:21:35 +0100

    intent and training_instance models created

---
+++ iva_backend/app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+import uuid
+
+
+class Intent(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    intent = models.CharField(max_length=128)
+
+
+class TrainingInstance(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    message_text = models.TextField()
+    intent = models.ForeignKey(Intent, on_delete=models.PROTECT, related_name='training_instances')
+    created_at = models.DateTimeField(auto_now_add=True)
