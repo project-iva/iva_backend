e7d99f8daefdbac1198f4e5c56f06b2e247304e9
Igor Pidik <igorPidik@gmail.com>
2021-06-29 18:42:21 +0200

    automatically subtract used ingredients

--- iva_backend/app/signals.py
+++ iva_backend/app/signals.py
@@ -3,6 +3,7 @@ from django.dispatch import receiver
 
 from iva_backend.app.iva_service.iva_service import IvaService
 from iva_backend.app.models import MindfulSession
+from iva_backend.app.models import MealTrackerEntry
 
 
 @receiver(post_save, sender=MindfulSession)
@@ -10,3 +11,13 @@ def mindful_session_saved_signal(sender, instance, created, **kwargs):
     if created:
         iva_service = IvaService()
         iva_service.send_mindful_session_created_notification()
+
+
+@receiver(post_save, sender=MealTrackerEntry)
+def subtract_used_ingredients(sender, instance, created, **kwargs):
+    if created:
+        # subtracts used up ingredients in the meal
+        for meal_ingredient in instance.meal.ingredients.all():
+            ingredient = meal_ingredient.ingredient
+            ingredient.amount -= meal_ingredient.amount
+            ingredient.save()
